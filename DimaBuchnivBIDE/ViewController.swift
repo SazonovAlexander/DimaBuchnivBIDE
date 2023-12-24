68b64ae0a10cc7c480f49bdbb595aa6a64ed5909
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2023-12-24 17:21:36 +0300

    Initial Commit

---
+++ DimaBuchnivBIDE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DimaBuchnivBIDE
+//
+//  Created by Александр Сазонов on 24.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
